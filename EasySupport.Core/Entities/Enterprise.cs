98461fb116da67982515a0c2c154ff08e91a17da
Henrique carvalho <henriqueart7@gmail.com>
2024-11-25 19:35:09 -0300

    criando Enterprise entity

---
+++ EasySupport.Core/Entities/Enterprise.cs
@@ -0,0 +1,17 @@
+﻿namespace EasySupport.Core.Entities
+{
+    public class Enterprise : BaseEntity
+    {
+        public Enterprise(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public void Update(string name)
+        {
+            Name = name;
+        }
+    }
+}
