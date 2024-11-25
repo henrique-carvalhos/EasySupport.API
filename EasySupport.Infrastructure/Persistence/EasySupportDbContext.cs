4c0bb76f867da76c6409742250ad1b64e2faaa2b
Henrique carvalho <henriqueart7@gmail.com>
2024-11-25 20:43:47 -0300

    definindo os DbSets

--- EasySupport.Infrastructure/Persistence/EasySupportDbContext.cs
+++ EasySupport.Infrastructure/Persistence/EasySupportDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EasySupport.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace EasySupport.Infrastructure.Persistence
 {
@@ -8,5 +9,15 @@ namespace EasySupport.Infrastructure.Persistence
         {
             
         }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Subcategory> Subcategories { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Enterprise> Enterprises { get; set; }
+        public DbSet<OriginService> OriginServices { get; set; }
+        public DbSet<StatusTicket> StatusTickets { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<TicketInteraction> TicketInteractions { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
