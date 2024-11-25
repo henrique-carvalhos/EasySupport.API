037227c9c7393a526a038b620332fd3f2896cc3a
Henrique carvalho <henriqueart7@gmail.com>
2024-11-25 19:33:14 -0300

    criando Department entity

---
+++ EasySupport.Core/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿namespace EasySupport.Core.Entities
+{
+    public class Department : BaseEntity
+    {
+        public Department(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public void Update(string name)
+        {
+            Name = name;
+        }
+    }
+}
