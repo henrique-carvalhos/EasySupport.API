72502c328e8cbc5844ff9963503f4aa47db9fb90
Henrique carvalho <henriqueart7@gmail.com>
2024-11-25 19:42:14 -0300

    criando OriginService entity

---
+++ EasySupport.Core/Entities/OriginService.cs
@@ -0,0 +1,17 @@
+﻿namespace EasySupport.Core.Entities
+{
+    public class OriginService : BaseEntity
+    {
+        public OriginService(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public void Update(string name)
+        {
+            Name = name;
+        }
+    }
+}
