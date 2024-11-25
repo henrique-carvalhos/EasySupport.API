ca430f3a87e3d5d87442e229d9925a6bc7a228f5
Henrique carvalho <henriqueart7@gmail.com>
2024-11-25 20:52:50 -0300

    implementando OriginServiceConfiguration

---
+++ EasySupport.Infrastructure/Persistence/Configurations/OriginServiceConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using EasySupport.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasySupport.Infrastructure.Persistence.Configurations
+{
+    public class OriginServiceConfiguration : IEntityTypeConfiguration<OriginService>
+    {
+        public void Configure(EntityTypeBuilder<OriginService> builder)
+        {
+            builder
+                .HasKey(e => e.Id);
+
+            builder
+                .Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(150);
+        }
+    }
+}
