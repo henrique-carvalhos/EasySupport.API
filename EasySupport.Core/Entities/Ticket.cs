30739589a6d126a0b608da4cb7d0686c05eee499
Henrique carvalho <henriqueart7@gmail.com>
2024-11-25 20:37:54 -0300

    criando Ticket entity

---
+++ EasySupport.Core/Entities/Ticket.cs
@@ -0,0 +1,31 @@
+﻿using EasySupport.Core.Emums;
+
+namespace EasySupport.Core.Entities
+{
+    public class Ticket : BaseEntity
+    {
+        public Ticket(int clientId, int categoryId, int subcategoryId, int statusTicketId, int prioridadeId, Priority priority, string description)
+        {
+            ClientId = clientId;
+            CategoryId = categoryId;
+            SubcategoryId = subcategoryId;
+            StatusTicketId = statusTicketId;
+            PrioridadeId = prioridadeId;
+            Priority = priority;
+            Description = description;
+        }
+
+        public int ClientId { get; private set; }
+        public int CategoryId { get; private set; }
+        public int SubcategoryId { get; private set; }
+        public int StatusTicketId { get; private set; }
+        public int PrioridadeId { get; private set; }
+        public Priority Priority { get; private set; }
+        public string Description { get; private set; }
+        public Category Category { get; set; }
+        public Subcategory Subcategory { get; set; }
+        public StatusTicket StatusTicket { get; set; }
+        public User Client { get; set; }
+        public List<TicketInteraction> Interactions { get; set; }
+    }
+}
