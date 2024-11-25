89c3a1c76ecb0ca76c082d22fdb54ce08dba44a0
Henrique carvalho <henriqueart7@gmail.com>
2024-11-25 19:51:07 -0300

    criando Subcategory entity

---
+++ EasySupport.Core/Entities/Subcategory.cs
@@ -0,0 +1,21 @@
+﻿namespace EasySupport.Core.Entities
+{
+    public class Subcategory : BaseEntity
+    {
+        public Subcategory(string name, int categoryId) : base()
+        {
+            Name = name;
+            CategoryId = categoryId;
+        }
+
+        public string Name { get; private set; }
+        public int CategoryId { get; private set; }
+        public Category Category { get; set; }
+
+        public void Update(string name, int categoryId)
+        {
+            Name = name;
+            CategoryId = categoryId;
+        }
+    }
+}
