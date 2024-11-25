780896eba30c04a9559e559435ccfa2688b31548
Henrique carvalho <henriqueart7@gmail.com>
2024-11-25 19:31:13 -0300

    criando user entity

---
+++ EasySupport.Core/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿namespace EasySupport.Core.Entities
+{
+    public class User : BaseEntity
+    {
+        public User(string name, string email, string role, DateTime alteredAt) : base()
+        {
+            Name = name;
+            Email = email;
+            Role = role;
+            AlteredAt = DateTime.Now;
+        }
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Role { get; private set; } // "Atendente" ou "Solicitante"
+        public DateTime AlteredAt { get; private set; }
+
+        public void Update(string name, string email, string role, DateTime alteredAt)
+        {
+            Name = name;
+            Email = email;
+            Role = role;
+            AlteredAt = DateTime.Now;
+        }
+    }
+}
