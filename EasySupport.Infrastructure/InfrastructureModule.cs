b5997c66abc7e01569eeb2a7043a46e3d5941f21
Henrique carvalho <henriqueart7@gmail.com>
2024-11-24 21:32:05 -0300

    criando e configurando InfrastructureModule

---
+++ EasySupport.Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EasySupport.Infrastructure
+{
+    public static class InfrastructureModule
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddRepositories()
+                .AddData(configuration);
+
+            return services;
+        }
+
+        private static IServiceCollection AddData(this IServiceCollection services, IConfiguration configuration)
+        {
+            //recuperar string de conexão com o banco de dados
+
+            return services;
+        }
+
+        private static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            //configurar injeção de dependencia das infertaces e seus respectivos repositórios
+
+            return services;
+        }
+    }
+}
