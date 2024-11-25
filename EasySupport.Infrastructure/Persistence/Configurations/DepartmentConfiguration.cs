6e079cb155f310c8df08e9bd0bfa4cd58cd1639d
Henrique carvalho <henriqueart7@gmail.com>
2024-11-25 20:50:47 -0300

    implementando DepartmentConfiguration

---
+++ EasySupport.Infrastructure/Persistence/Configurations/DepartmentConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using EasySupport.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasySupport.Infrastructure.Persistence.Configurations
+{
+    public class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder
+                .HasKey(e => e.Id);
+
+            builder
+                .Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(150);
+        }
+    }
+}
