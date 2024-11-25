c24dc1f7b410a02c7b96ee45b47a79891b84f591
Henrique carvalho <henriqueart7@gmail.com>
2024-11-25 20:29:35 -0300

    criando TicketInteraction

---
+++ EasySupport.Core/Entities/TicketInteraction.cs
@@ -0,0 +1,21 @@
+﻿namespace EasySupport.Core.Entities
+{
+    public class TicketInteraction
+    {
+        public TicketInteraction(int ticketId,int userId,string message, DateTime createdAt)
+        {
+            TicketId = ticketId;
+            UserId = userId;
+            Message = message;
+            CreatedAt = DateTime.Now;
+        }
+
+        public int Id { get; private set; }
+        public int TicketId { get; private set; }
+        public Ticket Ticket { get; set; }
+        public int UserId { get; private set; }
+        public User User { get; set; }
+        public string Message { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+    }
+}
