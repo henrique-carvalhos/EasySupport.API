b0c7bf9e9a2063bad7b46072dcdbea0d8356bdec
Henrique carvalho <henriqueart7@gmail.com>
2024-11-25 19:49:14 -0300

    criando category entity

---
+++ EasySupport.Core/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace EasySupport.Core.Entities
+{
+    public class Category : BaseEntity
+    {
+        public Category(string name)
+        {
+            Name = name;
+            Subcategories = [];
+        }
+
+        public string Name { get; private set; } // "Equipamento", "Programas", etc.
+        public List<Subcategory> Subcategories { get; set; }
+    }
+}
