1c4680475cfbaa7f0c48eb3d7d5292bd5faeaa95
Henrique carvalho <henriqueart7@gmail.com>
2024-11-24 21:21:38 -0300

    criando e configurando ApplicationModule

---
+++ EasySupport.Application/ApplicationModule.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace EasySupport.Application
+{
+    public static class ApplicationModule
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services
+                .AddHandlers()
+                .AddValidation();
+
+            return services;
+        }
+
+        public static IServiceCollection AddHandlers(this IServiceCollection services)
+        {
+            //configurar MediatR
+
+            return services;
+        }
+
+        public static IServiceCollection AddValidation(this IServiceCollection services)
+        {
+
+            //configurar fluentValidation
+
+            //Configurar IPipelineBehavior
+
+            return services;
+        }
+    }
+}
