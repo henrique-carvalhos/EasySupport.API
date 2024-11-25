934ed09d29c57a7d795df4a610526edd7335999b
Henrique carvalho <henriqueart7@gmail.com>
2024-11-24 21:29:38 -0300

    criando EasySupportDbContext

---
+++ EasySupport.Infrastructure/Persistence/EasySupportDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EasySupport.Infrastructure.Persistence
+{
+    public class EasySupportDbContext : DbContext
+    {
+        public EasySupportDbContext(DbContextOptions<EasySupportDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
