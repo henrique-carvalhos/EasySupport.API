b11fb06378d18618399aea02eef75b7569572906
Henrique carvalho <henriqueart7@gmail.com>
2024-11-25 22:38:04 -0300

    implementando TicketConfiguration

--- EasySupport.Core/Entities/Ticket.cs
+++ EasySupport.Core/Entities/Ticket.cs
@@ -4,22 +4,21 @@ namespace EasySupport.Core.Entities
 {
     public class Ticket : BaseEntity
     {
-        public Ticket(int clientId, int categoryId, int subcategoryId, int statusTicketId, int prioridadeId, Priority priority, string description)
+        public Ticket(int clientId, int categoryId, int subcategoryId, int statusTicketId, Priority priority, string description)
         {
             ClientId = clientId;
             CategoryId = categoryId;
             SubcategoryId = subcategoryId;
             StatusTicketId = statusTicketId;
-            PrioridadeId = prioridadeId;
             Priority = priority;
             Description = description;
+            Interactions = new List<TicketInteraction>();
         }
 
         public int ClientId { get; private set; }
         public int CategoryId { get; private set; }
         public int SubcategoryId { get; private set; }
         public int StatusTicketId { get; private set; }
-        public int PrioridadeId { get; private set; }
         public Priority Priority { get; private set; }
         public string Description { get; private set; }
         public Category Category { get; set; }
---
+++ EasySupport.Infrastructure/Persistence/Configurations/TicketConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using EasySupport.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasySupport.Infrastructure.Persistence.Configurations
+{
+    public class TicketConfiguration : IEntityTypeConfiguration<Ticket>
+    {
+        public void Configure(EntityTypeBuilder<Ticket> builder)
+        {
+            builder
+                .HasKey(t => t.Id);
+
+            builder
+                .Property(x => x.Description)
+                .IsRequired()
+                .HasMaxLength(255);
+
+
+            builder
+                .HasOne(c => c.Category)
+                .WithMany()
+                .HasForeignKey(c => c.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(s => s.Subcategory)
+                .WithMany()
+                .HasForeignKey(p => p.SubcategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(t => t.StatusTicket)
+                .WithMany()
+                .HasForeignKey(t =>  t.StatusTicketId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(u => u.Client)
+                .WithMany()
+                .HasForeignKey(u => u.ClientId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(t => t.Interactions)
+               .WithOne(t => t.Ticket)
+               .HasForeignKey(t => t.TicketId)
+               .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
