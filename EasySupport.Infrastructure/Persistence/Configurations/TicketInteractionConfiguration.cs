936d3ce8608828abfdb02be422a86822a2a1420b
Henrique carvalho <henriqueart7@gmail.com>
2024-11-25 22:19:41 -0300

    implementando TicketInteractionConfiguration

---
+++ EasySupport.Infrastructure/Persistence/Configurations/TicketInteractionConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using EasySupport.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasySupport.Infrastructure.Persistence.Configurations
+{
+    public class TicketInteractionConfiguration : IEntityTypeConfiguration<TicketInteraction>
+    {
+        public void Configure(EntityTypeBuilder<TicketInteraction> builder)
+        {
+            builder
+                .HasKey(t => t.Id);
+
+            builder
+                .Property(t => t.Message)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder
+                .HasOne(t => t.Ticket)
+                .WithMany(t => t.Interactions)
+                .HasForeignKey(t => t.TicketId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder
+                .HasOne(t => t.Attendant)
+                .WithMany()
+                .HasForeignKey(t => t.AttendantId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
