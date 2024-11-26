d423a922197b5949b090d8b64d7c5fe813061050
Henrique carvalho <henriqueart7@gmail.com>
2024-11-25 22:09:45 -0300

    implementando SubcategoryConfiguration

---
+++ EasySupport.Infrastructure/Persistence/Configurations/SubcategoryConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using EasySupport.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasySupport.Infrastructure.Persistence.Configurations
+{
+    public class SubcategoryConfiguration : IEntityTypeConfiguration<Subcategory>
+    {
+        public void Configure(EntityTypeBuilder<Subcategory> builder)
+        {
+            builder.
+                HasKey(i => i.Id);
+
+            builder
+                .Property(i => i.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder
+                .HasOne(c => c.Category)
+                .WithMany(c => c.Subcategories)
+                .HasForeignKey(c => c.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
