b298a4d54e519fc3b119b926700e33f81d3765c3
Henrique carvalho <henriqueart7@gmail.com>
2024-11-25 22:04:08 -0300

    implementando CategoryConfiguration

---
+++ EasySupport.Infrastructure/Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using EasySupport.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasySupport.Infrastructure.Persistence.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder
+                .HasKey(x => x.Id);
+
+            builder
+                .Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(150);
+        }
+    }
+}
