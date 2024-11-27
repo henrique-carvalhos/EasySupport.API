a75eca09d8e01ba579cb9fdb8cb9289de54aa499
Henrique carvalho <henriqueart7@gmail.com>
2024-11-27 19:19:17 -0300

    criando interface ICategoryRepository

---
+++ EasySupport.Core/Repositories/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using EasySupport.Core.Entities;
+
+namespace EasySupport.Core.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<Category?> GetByIdAsync(int id);
+        Task<List<Category>> GetAllAsync(string search);
+        Task<int> AddAsync(Category category);
+        Task UpdateAsync(Category category);
+    }
+}
