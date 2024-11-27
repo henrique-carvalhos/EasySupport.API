b43d5b1da8c4e4fa9657c508b8435b16f413af36
Henrique carvalho <henriqueart7@gmail.com>
2024-11-27 19:35:48 -0300

    criando CategoriesController

---
+++ EasySupport.API/Controllers/CategoriesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EasySupport.Core.Entities;
+using EasySupport.Infrastructure.Persistence;
+using MediatR;
+
+namespace EasySupport.API.Controllers
+{
+    [Route("api/categories")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CategoriesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var query = new GetCategoryByIdQuery(id);
+
+            var result = await _mediator.Send(query);
+
+            if (result is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+    }
+}
