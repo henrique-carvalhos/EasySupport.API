20b2a4305690b82cadf1b23664b9fd2366113a59
Henrique carvalho <henriqueart7@gmail.com>
2024-11-27 19:26:07 -0300

    implementando CategoryRepository

---
+++ EasySupport.Infrastructure/Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using EasySupport.Core.Entities;
+using EasySupport.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasySupport.Infrastructure.Persistence.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly EasySupportDbContext _context;
+        public CategoryRepository(EasySupportDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> AddAsync(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+
+            return category.Id;
+        }
+
+        public async Task<List<Category>> GetAllAsync(string search)
+        {
+            var category = await _context
+                .Categories
+                .ToListAsync();
+
+            return category;
+        }
+
+        public async Task<Category?> GetByIdAsync(int id)
+        {
+            return await _context
+                .Categories
+                .SingleOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task UpdateAsync(Category category)
+        {
+            category.Update(category.Name);
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
