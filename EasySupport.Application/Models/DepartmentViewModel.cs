ff1af582b318e9b9f6cc2b01970e9cb35c608d4c
Henrique carvalho <henriqueart7@gmail.com>
2024-11-28 19:48:03 -0300

    criando DepartmentViewModel

---
+++ EasySupport.Application/Models/DepartmentViewModel.cs
@@ -0,0 +1,21 @@
+﻿using EasySupport.Core.Entities;
+
+namespace EasySupport.Application.Models
+{
+    public class DepartmentViewModel
+    {
+        public DepartmentViewModel(int id, string name, bool isDeleted)
+        {
+            Id = id;
+            Name = name;
+            IsDeleted = isDeleted;
+        }
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public bool IsDeleted { get; private set; }
+
+        public static DepartmentViewModel FromEntity(Department department)
+            => new(department.Id, department.Name, department.IsDeleted);
+    }
+}
