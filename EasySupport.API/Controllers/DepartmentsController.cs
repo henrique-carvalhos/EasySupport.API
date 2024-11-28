91e1bd7b3ad6c58854e694c0a1e5613452e88947
Henrique carvalho <henriqueart7@gmail.com>
2024-11-28 19:50:07 -0300

    criando DepartmentsController

---
+++ EasySupport.API/Controllers/DepartmentsController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasySupport.API.Controllers
+{
+    [Route("api/departments")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public DepartmentsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+    }
+}
