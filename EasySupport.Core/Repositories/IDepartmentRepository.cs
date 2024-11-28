40515e70671051b98e0a2952b2ea2758e74f97d0
Henrique carvalho <henriqueart7@gmail.com>
2024-11-28 20:00:28 -0300

    criando e implementando IDepartmentRepository

---
+++ EasySupport.Core/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,12 @@
+﻿using EasySupport.Core.Entities;
+
+namespace EasySupport.Core.Repositories
+{
+    public interface IDepartmentRepository
+    {
+        Task<Department?> GetByIdAsync(int id);
+        Task<List<Department>> GetAllAsync(string search);
+        Task<int> AddAsync(Department department);
+        Task UpdateAsync(Department department);
+    }
+}
