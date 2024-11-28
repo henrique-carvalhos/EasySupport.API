787b038771e196024f0e70af58855fad63c50445
Henrique carvalho <henriqueart7@gmail.com>
2024-11-27 21:14:31 -0300

    implementando SubcategoriesRepository

---
+++ EasySupport.Infrastructure/Persistence/Repositories/SubcategoriesRepository.cs
@@ -0,0 +1,47 @@
+﻿using EasySupport.Core.Entities;
+using EasySupport.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasySupport.Infrastructure.Persistence.Repositories
+{
+    public class SubcategoriesRepository : ISubcategoriesRepository
+    {
+        private readonly EasySupportDbContext _context;
+        public SubcategoriesRepository(EasySupportDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> AddAsync(Subcategory subcategory)
+        {
+            _context.Subcategories.Add(subcategory);
+            await _context.SaveChangesAsync();
+
+            return subcategory.Id;
+        }
+
+        public async Task<List<Subcategory>> GetAllAsync(string search)
+        {
+            var subcategories = await _context
+                .Subcategories
+                .Include(c => c.Category)
+                .ToListAsync();
+
+            return subcategories;
+        }
+
+        public async Task<Subcategory?> GetByIdAsync(int id)
+        {
+            return await _context
+                .Subcategories
+                .Include(c => c.Category)
+                .SingleOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task UpdateAsync(Subcategory subcategory)
+        {
+            _context.Update(subcategory);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
