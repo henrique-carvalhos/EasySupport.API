a475775a9df1f3d58f9b2a8c281099f1a7e92a2a
Henrique carvalho <henriqueart7@gmail.com>
2024-11-27 21:07:28 -0300

    criando SubcategoriesController

---
+++ EasySupport.API/Controllers/SubcategoriesController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasySupport.API.Controllers
+{
+    [Route("api/subcategories")]
+    [ApiController]
+    public class SubcategoriesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public SubcategoriesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
