aea4af957092476624ba6de59d91affd64ee0c06
Henrique carvalho <henriqueart7@gmail.com>
2024-11-27 21:09:03 -0300

    criando ISubcategoriesRepository

---
+++ EasySupport.Core/Repositories/ISubcategoriesRepository.cs
@@ -0,0 +1,12 @@
+﻿using EasySupport.Core.Entities;
+
+namespace EasySupport.Core.Repositories
+{
+    public interface ISubcategoriesRepository
+    {
+        Task<Subcategory?> GetByIdAsync(int id);
+        Task<List<Subcategory>> GetAllAsync(string search);
+        Task<int> AddAsync(Subcategory subcategory);
+        Task UpdateAsync(Subcategory subcategory);
+    }
+}
