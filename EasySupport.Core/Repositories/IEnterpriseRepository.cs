1de6c648d9f94bd05d90c166e002a2c7d5100697
Henrique carvalho <henriqueart7@gmail.com>
2024-11-29 20:01:08 -0300

    implementando IEnterpriseRepository

---
+++ EasySupport.Core/Repositories/IEnterpriseRepository.cs
@@ -0,0 +1,12 @@
+﻿using EasySupport.Core.Entities;
+
+namespace EasySupport.Core.Repositories
+{
+    public interface IEnterpriseRepository
+    {
+        Task<Enterprise?> GetByIdAsync(int id);
+        Task<List<Enterprise>> GetAllAsync(string search);
+        Task<int> AddAsync(Enterprise enterprise);
+        Task UpdateAsync(Enterprise enterprise);
+    }
+}
