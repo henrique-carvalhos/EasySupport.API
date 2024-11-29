22da50faaf8b27a752e6cb65ab8bb07d1ad7dd5a
Henrique carvalho <henriqueart7@gmail.com>
2024-11-29 19:58:01 -0300

    criando EnterprisesController

---
+++ EasySupport.API/Controllers/EnterprisesController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasySupport.API.Controllers
+{
+    [Route("api/enterprises")]
+    [ApiController]
+    public class EnterprisesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public EnterprisesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
