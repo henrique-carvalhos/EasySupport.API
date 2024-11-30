533d31dec6e45c9ba4c6a17a07a35b05baa1a26d
Henrique carvalho <henriqueart7@gmail.com>
2024-11-30 18:45:57 -0300

    cirando OriginServicesController

---
+++ EasySupport.API/Controllers/OriginServicesController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasySupport.API.Controllers
+{
+    [Route("api/originservices")]
+    [ApiController]
+    public class OriginServicesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public OriginServicesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
