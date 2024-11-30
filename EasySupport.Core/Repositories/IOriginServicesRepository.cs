1adb9f64bc62d17c22eaa3d017931104e2141b57
Henrique carvalho <henriqueart7@gmail.com>
2024-11-30 18:52:31 -0300

    implementando IOriginServicesRepository

---
+++ EasySupport.Core/Repositories/IOriginServicesRepository.cs
@@ -0,0 +1,12 @@
+﻿using EasySupport.Core.Entities;
+
+namespace EasySupport.Core.Repositories
+{
+    public interface IOriginServicesRepository
+    {
+        Task<OriginService?> GetByIdAsync(int id);
+        Task<List<OriginService>> GetAllAsync(string search);
+        Task<int> AddAsync(OriginService originService);
+        Task UpdateAsync(OriginService originService);
+    }
+}
