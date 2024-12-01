49caf8705521eb3501d2faf331223cd1c709eb24
Henrique carvalho <henriqueart7@gmail.com>
2024-12-01 18:39:22 -0300

    implementando IStatusTicketRepository

---
+++ EasySupport.Core/Repositories/IStatusTicketRepository.cs
@@ -0,0 +1,12 @@
+﻿using EasySupport.Core.Entities;
+
+namespace EasySupport.Core.Repositories
+{
+    public interface IStatusTicketRepository
+    {
+        Task<StatusTicket?> GetByIdAsync(int id);
+        Task<List<StatusTicket>> GetAllAsync(string search);
+        Task<int> AddAsync(StatusTicket status);
+        Task UpdateAsync(StatusTicket status);
+    }
+}
