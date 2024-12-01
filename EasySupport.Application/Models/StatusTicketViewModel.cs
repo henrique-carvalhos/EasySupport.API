5beb20bddbabea5ef2867559b662cd18f9635d6d
Henrique carvalho <henriqueart7@gmail.com>
2024-12-01 18:32:16 -0300

    criando StatusTicketViewModel

---
+++ EasySupport.Application/Models/StatusTicketViewModel.cs
@@ -0,0 +1,21 @@
+﻿using EasySupport.Core.Entities;
+
+namespace EasySupport.Application.Models
+{
+    public class StatusTicketViewModel
+    {
+        public StatusTicketViewModel(int id, string name, bool isDeleted)
+        {
+            Id = id;
+            Name = name;
+            IsDeleted = isDeleted;
+        }
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public bool IsDeleted { get; private set; }
+
+        public static StatusTicketViewModel FromEntity(StatusTicket status)
+            => new(status.Id, status.Name, status.IsDeleted);
+    }
+}
