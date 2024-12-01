c878e45d6df0eb730545bc8b740c538a7f657e12
Henrique carvalho <henriqueart7@gmail.com>
2024-12-01 18:29:38 -0300

    criando StatusTicketsController

---
+++ EasySupport.API/Controllers/StatusTicketsController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasySupport.API.Controllers
+{
+    [Route("api/statustickets")]
+    [ApiController]
+    public class StatusTicketsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public StatusTicketsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
