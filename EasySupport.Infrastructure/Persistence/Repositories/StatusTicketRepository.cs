914151ac9555acfa9a47bd805d19398955f9a34e
Henrique carvalho <henriqueart7@gmail.com>
2024-12-01 19:14:15 -0300

    implementando StatusTicketRepository

---
+++ EasySupport.Infrastructure/Persistence/Repositories/StatusTicketRepository.cs
@@ -0,0 +1,47 @@
+﻿using EasySupport.Core.Entities;
+using EasySupport.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasySupport.Infrastructure.Persistence.Repositories
+{
+    public class StatusTicketRepository : IStatusTicketRepository
+    {
+        private readonly EasySupportDbContext _context;
+        public StatusTicketRepository(EasySupportDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> AddAsync(StatusTicket status)
+        {
+            _context.StatusTickets.Add(status);
+            await _context.SaveChangesAsync();
+
+            return status.Id;
+        }
+
+        public async Task<List<StatusTicket>> GetAllAsync(string search)
+        {
+            var status = await _context
+                .StatusTickets
+                .Where(s => !s.IsDeleted && (search == "" || s.Name.Contains(search)))
+                .ToListAsync();
+
+            return status;
+        }
+
+        public async Task<StatusTicket?> GetByIdAsync(int id)
+        {
+            return await _context
+                .StatusTickets
+                .Where(s => !s.IsDeleted)
+                .SingleOrDefaultAsync(s => s.Id == id);
+        }
+
+        public async Task UpdateAsync(StatusTicket status)
+        {
+            _context.Update(status);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
