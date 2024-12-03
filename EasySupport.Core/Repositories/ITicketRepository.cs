a461a2883eb49212a0c7e43a63875e13a56761e5
Henrique carvalho <henriqueart7@gmail.com>
2024-12-03 20:12:43 -0300

    implementando ITicketRepository

---
+++ EasySupport.Core/Repositories/ITicketRepository.cs
@@ -0,0 +1,12 @@
+﻿using EasySupport.Core.Entities;
+
+namespace EasySupport.Core.Repositories
+{
+    public interface ITicketRepository
+    {
+        Task<Ticket?> GetByIdAsync(int id);
+        Task<List<Ticket>> GetAllAsync(string search);
+        Task<int> AddAsync(Ticket ticket);
+        Task UpdateAsync(Ticket ticket);
+    }
+}
