9117ba322f5e9fce2474dac3095b76b002117465
Henrique carvalho <henriqueart7@gmail.com>
2024-12-05 20:38:56 -0300

    implementando ITicketInteractionRepository

---
+++ EasySupport.Core/Repositories/ITicketInteractionRepository.cs
@@ -0,0 +1,10 @@
+﻿using EasySupport.Core.Entities;
+
+namespace EasySupport.Core.Repositories
+{
+    public interface ITicketInteractionRepository
+    {
+        Task<List<TicketInteraction>> GetAllInteractionsAsync(int ticketId);
+        Task<int> AddAsync(TicketInteraction category);
+    }
+}
