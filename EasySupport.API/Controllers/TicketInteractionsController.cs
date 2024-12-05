969b478e9f907ea17caaeaf83abd9419c5931ac8
Henrique carvalho <henriqueart7@gmail.com>
2024-12-05 20:32:29 -0300

    criando TicketInteractionsController

---
+++ EasySupport.API/Controllers/TicketInteractionsController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasySupport.API.Controllers
+{
+    [Route("api/ticketInteractions")]
+    [ApiController]
+    public class TicketInteractionsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public TicketInteractionsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
