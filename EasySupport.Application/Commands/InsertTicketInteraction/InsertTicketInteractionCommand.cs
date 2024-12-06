1d9f1f9306bf4e58031c70f2ea9484b3b8f975e2
Henrique carvalho <henriqueart7@gmail.com>
2024-12-05 22:08:19 -0300

    implementando InsertTicketInteractionCommand

---
+++ EasySupport.Application/Commands/InsertTicketInteraction/InsertTicketInteractionCommand.cs
@@ -0,0 +1,17 @@
+﻿using EasySupport.Application.Models;
+using EasySupport.Core.Entities;
+using MediatR;
+
+namespace EasySupport.Application.Commands.InsertTicketInteraction
+{
+    public class InsertTicketInteractionCommand : IRequest<ResultViewModel<int>>
+    {
+        public int TicketId { get;  set; }
+        public int AttendantId { get;  set; }
+        public string Message { get;  set; }
+        public DateTime CreatedAt { get;  set; }
+
+        public TicketInteraction ToEntity()
+            => new(TicketId, AttendantId, Message, CreatedAt);
+    }
+}
---
+++ EasySupport.Application/Commands/InsertTicketInteraction/InsertTicketInteractionCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using EasySupport.Application.Models;
+using EasySupport.Core.Repositories;
+using MediatR;
+
+namespace EasySupport.Application.Commands.InsertTicketInteraction
+{
+    public class InsertTicketInteractionCommandHandler : IRequestHandler<InsertTicketInteractionCommand, ResultViewModel<int>>
+    {
+        private readonly ITicketInteractionRepository _repository;
+        public InsertTicketInteractionCommandHandler(ITicketInteractionRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ResultViewModel<int>> Handle(InsertTicketInteractionCommand request, CancellationToken cancellationToken)
+        {
+            var interaction = request.ToEntity();
+
+            await _repository.AddAsync(interaction);
+
+            return ResultViewModel<int>.Success(interaction.Id);
+        }
+    }
+}
