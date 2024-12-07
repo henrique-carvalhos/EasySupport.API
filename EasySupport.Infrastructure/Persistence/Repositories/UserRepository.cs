8f5ddce0b3732a61686ddbe5151512069c64ebfe
Henrique carvalho <henriqueart7@gmail.com>
2024-12-06 23:26:00 -0300

    implementando UserRepository

---
+++ EasySupport.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using EasySupport.Core.Entities;
+using EasySupport.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasySupport.Infrastructure.Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly EasySupportDbContext _context;
+        public UserRepository(EasySupportDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> AddAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user.Id;
+        }
+
+        public async Task<List<User>> GetAllAsync(string search)
+        {
+            var users = await _context
+                .Users
+                .Include(e => e.Enterprise)
+                .Include(d => d.Department)
+                .Where(u => !u.IsDeleted && (search == "" || u.Name.Contains(search)))
+                .ToListAsync();
+
+            return users;
+        }
+
+        public async Task<User?> GetByIdAsync(int id)
+        {
+            return await _context
+                .Users
+                .Include(e => e.Enterprise)
+                .Include(d => d.Department)
+                .SingleOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
