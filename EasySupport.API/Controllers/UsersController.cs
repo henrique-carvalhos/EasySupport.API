ad6f56c9389d354651492b5ea09a04ca015fdcba
Henrique carvalho <henriqueart7@gmail.com>
2024-12-06 23:23:36 -0300

    criando UsersController

---
+++ EasySupport.API/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasySupport.API.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
