405ac928be37e9cba2c3c78b0e3f5059ceb31fac
Henrique carvalho <henriqueart7@gmail.com>
2024-12-06 23:25:07 -0300

    implementando IUserRepository

---
+++ EasySupport.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using EasySupport.Core.Entities;
+
+namespace EasySupport.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(int id);
+        Task<List<User>> GetAllAsync(string search);
+        Task<int> AddAsync(User user);
+        Task UpdateAsync(User user);
+    }
+}
