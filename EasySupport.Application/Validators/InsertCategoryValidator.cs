ca3cb9701e739decc3f0df38c8116373f48d4e38
Henrique carvalho <henriqueart7@gmail.com>
2024-12-08 18:34:34 -0300

    implementando InsertCategoryValidator

---
+++ EasySupport.Application/Validators/InsertCategoryValidator.cs
@@ -0,0 +1,17 @@
+﻿using EasySupport.Application.Commands.InsertCategory;
+using FluentValidation;
+
+namespace EasySupport.Application.Validators
+{
+    public class InsertCategoryValidator : AbstractValidator<InsertCategoryCommand>
+    {
+        public InsertCategoryValidator()
+        {
+            RuleFor(d => d.Name)
+                .NotEmpty()
+                .WithMessage("Nome não pode ser vazio")
+                .MaximumLength(150)
+                .WithMessage("Tamanho máximo é de 150 caracteres");
+        }
+    }
+}
