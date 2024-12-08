8b5be8c792e1309b7763078521f8c2be0d40b500
Henrique carvalho <henriqueart7@gmail.com>
2024-12-08 18:54:19 -0300

    implementando InsertTicketInteractionValidator

---
+++ EasySupport.Application/Validators/InsertTicketInteractionValidator.cs
@@ -0,0 +1,17 @@
+﻿using EasySupport.Application.Commands.InsertTicketInteraction;
+using FluentValidation;
+
+namespace EasySupport.Application.Validators
+{
+    public class InsertTicketInteractionValidator : AbstractValidator<InsertTicketInteractionCommand>
+    {
+        public InsertTicketInteractionValidator()
+        {
+            RuleFor(i => i.Message)
+                .NotEmpty()
+                .WithMessage("Mensagem não pode ser vazio")
+                .MaximumLength(255)
+                .WithMessage("Tamanho máximo é de 255 caracteres");
+        }
+    }
+}
