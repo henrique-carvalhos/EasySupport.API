4fb9dc1b14951762f7811b87e4531e5f8796f11b
Henrique carvalho <henriqueart7@gmail.com>
2024-12-08 15:02:52 -0300

    implementando GetUserByIdQuery

---
+++ EasySupport.Application/Queries/GetUserById/GetUserByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using EasySupport.Application.Models;
+using MediatR;
+
+namespace EasySupport.Application.Queries.GetUserById
+{
+    public class GetUserByIdQuery : IRequest<ResultViewModel<UserViewModel>>
+    {
+        public GetUserByIdQuery(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+    }
+}
---
+++ EasySupport.Application/Queries/GetUserById/GetUserByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using EasySupport.Application.Models;
+using EasySupport.Core.Repositories;
+using MediatR;
+
+namespace EasySupport.Application.Queries.GetUserById
+{
+    public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, ResultViewModel<UserViewModel>>
+    {
+        private readonly IUserRepository _repository;
+        public GetUserByIdQueryHandler(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ResultViewModel<UserViewModel>> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _repository.GetByIdAsync(request.Id);
+
+            if (user is null)
+            {
+                return ResultViewModel<UserViewModel>.Error("Usuário não encontrado");
+            }
+
+            var model = UserViewModel.FromEntity(user);
+
+            return ResultViewModel<UserViewModel>.Success(model);
+        }
+    }
+}
