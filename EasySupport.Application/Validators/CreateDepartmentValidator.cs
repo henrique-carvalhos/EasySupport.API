088ba77d20a9855086d3b20a26ac66091938ca2b
Henrique carvalho <henriqueart7@gmail.com>
2024-12-08 18:27:13 -0300

    implementando CreateDepartmentValidator

---
+++ EasySupport.Application/Validators/CreateDepartmentValidator.cs
@@ -0,0 +1,18 @@
+﻿using EasySupport.Application.Commands.InsertDepartment;
+using FluentValidation;
+
+namespace EasySupport.Application.Validators
+{
+    public class CreateDepartmentValidator : AbstractValidator<InsertDepartmentCommand>
+    {
+        public CreateDepartmentValidator()
+        {
+            RuleFor(d => d.Name)
+                .NotEmpty()
+                .WithMessage("Nome não pode ser vazio")
+                .MaximumLength(150)
+                .WithMessage("Tamanho máximo é de 50 caracteres");
+
+        }
+    }
+}
