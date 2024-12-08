d802982160c826e164d0414bef7831f3b35aa19f
Henrique carvalho <henriqueart7@gmail.com>
2024-12-08 18:47:19 -0300

    implementando InsertUserValidator

---
+++ EasySupport.Application/Validators/InsertUserValidator.cs
@@ -0,0 +1,30 @@
+﻿using EasySupport.Application.Commands.InsertUser;
+using FluentValidation;
+
+namespace EasySupport.Application.Validators
+{
+    public class InsertUserValidator : AbstractValidator<InsertUserCommand>
+    {
+        public InsertUserValidator()
+        {
+            RuleFor(u => u.Name)
+                .NotEmpty()
+                .WithMessage("Nome não pode ser vazio")
+                .MaximumLength(150)
+                .WithMessage("Tamanho máximo é de 150 caracteres");
+
+            RuleFor(u => u.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .WithMessage("E-mail não pode ser vazio")
+                .MaximumLength(150)
+                .WithMessage("Tamanho máximo é de 150 caracteres");
+
+            RuleFor(u => u.Role)
+                .NotEmpty()
+                .WithMessage("Role não pode ser vazio")
+                .MaximumLength(50)
+                .WithMessage("Tamanho máximo é de 50 caracteres");
+        }
+    }
+}
