741137146a2bb0e72276dd916e5a1e089733bdca
Henrique carvalho <henriqueart7@gmail.com>
2024-12-08 19:03:22 -0300

    implementando ValidateInsertSubcategoryCommandBehavior

---
+++ EasySupport.Application/Commands/InsertSubcategories/ValidateInsertSubcategoryCommandBehavior.cs
@@ -0,0 +1,28 @@
+﻿using EasySupport.Application.Models;
+using EasySupport.Infrastructure.Persistence;
+using MediatR;
+
+namespace EasySupport.Application.Commands.InsertSubcategories
+{
+    public class ValidateInsertSubcategoryCommandBehavior
+        : IPipelineBehavior<InsertSubcategoriesCommand, ResultViewModel<int>>
+    {
+        private readonly EasySupportDbContext _context;
+        public ValidateInsertSubcategoryCommandBehavior(EasySupportDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel<int>> Handle(InsertSubcategoriesCommand request, RequestHandlerDelegate<ResultViewModel<int>> next, CancellationToken cancellationToken)
+        {
+            var categoryExists =  _context.Categories.Any(c => c.Id == request.CategoryId);
+
+            if (!categoryExists)
+            {
+                return ResultViewModel<int>.Error($"Categoria com identificador {request.CategoryId} é inválida");
+            }
+
+            return await next();
+        }
+    }
+}
