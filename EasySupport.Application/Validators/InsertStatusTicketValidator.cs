c8357eb6629bc91244b753458bc3622f447e29a3
Henrique carvalho <henriqueart7@gmail.com>
2024-12-08 18:38:18 -0300

    implementando InsertStatusTicketValidator

---
+++ EasySupport.Application/Validators/InsertStatusTicketValidator.cs
@@ -0,0 +1,17 @@
+﻿using EasySupport.Application.Commands.InsertStatusTicket;
+using FluentValidation;
+
+namespace EasySupport.Application.Validators
+{
+    public class InsertStatusTicketValidator : AbstractValidator<InsertStatusTicketCommand>
+    {
+        public InsertStatusTicketValidator()
+        {
+            RuleFor(d => d.Name)
+                .NotEmpty()
+                .WithMessage("Nome não pode ser vazio")
+                .MaximumLength(150)
+                .WithMessage("Tamanho máximo é de 150 caracteres");
+        }
+    }
+}
