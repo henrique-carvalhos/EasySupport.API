29aafbe75a0f2b29a516d197ee06957e28080228
Henrique carvalho <henriqueart7@gmail.com>
2024-12-08 18:43:38 -0300

    implementando InsertSubcategoryValidator

---
+++ EasySupport.Application/Validators/InsertSubcategoryValidator.cs
@@ -0,0 +1,17 @@
+﻿using EasySupport.Application.Commands.InsertSubcategories;
+using FluentValidation;
+
+namespace EasySupport.Application.Validators
+{
+    public class InsertSubcategoryValidator : AbstractValidator<InsertSubcategoriesCommand>
+    {
+        public InsertSubcategoryValidator()
+        {
+            RuleFor(d => d.Name)
+                .NotEmpty()
+                .WithMessage("Nome não pode ser vazio")
+                .MaximumLength(50)
+                .WithMessage("Tamanho máximo é de 50 caracteres");
+        }
+    }
+}
