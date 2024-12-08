245d8e809b83f22e3d2f52d69359921232b9bb8c
Henrique carvalho <henriqueart7@gmail.com>
2024-12-08 18:57:50 -0300

    implementando InsertTicketValidator

---
+++ EasySupport.Application/Validators/InsertTicketValidator.cs
@@ -0,0 +1,17 @@
+﻿using EasySupport.Application.Commands.InsertTicket;
+using FluentValidation;
+
+namespace EasySupport.Application.Validators
+{
+    public class InsertTicketValidator : AbstractValidator<InsertTicketCommand>
+    {
+        public InsertTicketValidator()
+        {
+            RuleFor(t => t.Description)
+                .NotEmpty()
+                .WithMessage("Descrição não pode ser vazio")
+                .MaximumLength(255)
+                .WithMessage("Tamanho máximo é de 255 caracteres");
+        }
+    }
+}
