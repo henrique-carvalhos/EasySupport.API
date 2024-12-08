cb0ad4158f319c7d5e601884fa5e907c8f5c345d
Henrique carvalho <henriqueart7@gmail.com>
2024-12-08 14:04:19 -0300

    add migration Enterprise e Deparment para o User

---
+++ EasySupport.Infrastructure/Persistence/Migrations/20241207023553_AddAtributosUser.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasySupport.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAtributosUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 1);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EnterpriseId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 1);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_DepartmentId",
+                table: "Users",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_EnterpriseId",
+                table: "Users",
+                column: "EnterpriseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Departments_DepartmentId",
+                table: "Users",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Enterprises_EnterpriseId",
+                table: "Users",
+                column: "EnterpriseId",
+                principalTable: "Enterprises",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Departments_DepartmentId",
+                table: "Users");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Enterprises_EnterpriseId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_DepartmentId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_EnterpriseId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "EnterpriseId",
+                table: "Users");
+        }
+    }
+}
