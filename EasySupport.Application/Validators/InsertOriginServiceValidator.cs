0e4ef0a9bf5453627b5a01a1b28161759e3dae19
Henrique carvalho <henriqueart7@gmail.com>
2024-12-08 18:36:56 -0300

    implementando InsertOriginServiceValidator

---
+++ EasySupport.Application/Validators/InsertOriginServiceValidator.cs
@@ -0,0 +1,17 @@
+﻿using EasySupport.Application.Commands.InsertOriginService;
+using FluentValidation;
+
+namespace EasySupport.Application.Validators
+{
+    public class InsertOriginServiceValidator : AbstractValidator<InsertOriginServiceCommand>
+    {
+        public InsertOriginServiceValidator()
+        {
+            RuleFor(d => d.Name)
+                .NotEmpty()
+                .WithMessage("Nome não pode ser vazio")
+                .MaximumLength(150)
+                .WithMessage("Tamanho máximo é de 150 caracteres");
+        }
+    }
+}
