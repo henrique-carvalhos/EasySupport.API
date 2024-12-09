d891094c7a1cb49b74c15e87c6c3ff626698fc50
Henrique carvalho <henriqueart7@gmail.com>
2024-12-09 18:59:26 -0300

    inserindo TicketCreatedNotification no insert do ticket

--- EasySupport.Application/Commands/InsertTicket/InsertTicketCommandHandler.cs
+++ EasySupport.Application/Commands/InsertTicket/InsertTicketCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using EasySupport.Application.Models;
+using EasySupport.Application.Notification.TicketCreated;
 using EasySupport.Core.Repositories;
 using MediatR;
 
@@ -7,9 +8,11 @@ namespace EasySupport.Application.Commands.InsertTicket
     public class InsertTicketCommandHandler : IRequestHandler<InsertTicketCommand, ResultViewModel<int>>
     {
         private readonly ITicketRepository _repository;
-        public InsertTicketCommandHandler(ITicketRepository repository)
+        private readonly IMediator _mediator;
+        public InsertTicketCommandHandler(ITicketRepository repository, IMediator mediator)
         {
             _repository = repository;
+            _mediator = mediator;
         }
 
         public async Task<ResultViewModel<int>> Handle(InsertTicketCommand request, CancellationToken cancellationToken)
@@ -17,6 +20,21 @@ namespace EasySupport.Application.Commands.InsertTicket
             var ticket = request.ToEntity();
 
             await _repository.AddAsync(ticket);
+
+            var ticketResult = await _repository.GetByIdAsync(ticket.Id);
+
+            var ticketCreated = new TicketCreatedNotification(
+                ticketResult.Id,
+                ticketResult.CreatedAt,
+                ticketResult.Client.Name, 
+                ticketResult.Client.Email,
+                ticketResult.Category.Name, 
+                ticketResult.Subcategory.Name,
+                ticketResult.StatusTicket.Name,
+                ticketResult.Priority.ToString(),
+                ticketResult.Description);
+
+            await _mediator.Publish(ticketCreated);
 
             return ResultViewModel<int>.Success(ticket.Id);
         }
