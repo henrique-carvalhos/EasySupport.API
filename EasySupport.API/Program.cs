e7a08a4731131221a233e9b8e06ffdc749b6d25f
Henrique carvalho <henriqueart7@gmail.com>
2024-12-09 18:59:44 -0300

    obtem chave da apiKey

--- EasySupport.API/Program.cs
+++ EasySupport.API/Program.cs
@@ -1,5 +1,6 @@
 using EasySupport.Application;
 using EasySupport.Infrastructure;
+using SendGrid.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddApplication()
     .AddInfrastructure(builder.Configuration);
+
+var sendGridApiKey = builder.Configuration.GetSection("SendGridApiKey").Value;
+builder.Services.AddSendGrid(a => a.ApiKey = sendGridApiKey);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
