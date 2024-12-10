c2b0d94ed5dd042a1dae0a2d99059f59edcd7e16
Henrique carvalho <henriqueart7@gmail.com>
2024-12-10 17:02:37 -0300

    add migration AddAttendantTicket

---
+++ EasySupport.Infrastructure/Persistence/Migrations/20241210195241_AddAttedantTicket.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasySupport.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAttedantTicket : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AttendantId",
+                table: "Tickets",
+                type: "int",
+                nullable: true,
+                defaultValue: null);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_AttendantId",
+                table: "Tickets",
+                column: "AttendantId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Users_AttendantId",
+                table: "Tickets",
+                column: "AttendantId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Users_AttendantId",
+                table: "Tickets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tickets_AttendantId",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "AttendantId",
+                table: "Tickets");
+        }
+    }
+}
