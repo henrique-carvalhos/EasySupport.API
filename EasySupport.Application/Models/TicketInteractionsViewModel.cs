5556fff972f7af4cd4249c3dca449721be3f717e
Henrique carvalho <henriqueart7@gmail.com>
2024-12-11 13:11:21 -0300

    incluindo role do Attendent na TicketInteractionsViewModel

--- EasySupport.Application/Models/TicketInteractionsViewModel.cs
+++ EasySupport.Application/Models/TicketInteractionsViewModel.cs
@@ -4,12 +4,13 @@ namespace EasySupport.Application.Models
 {
     public class TicketInteractionsViewModel
     {
-        public TicketInteractionsViewModel(int id, int ticketId, int attendantId, string attendantName, string message, DateTime createdAt)
+        public TicketInteractionsViewModel(int id, int ticketId, int attendantId, string attendantName, string role, string message, DateTime createdAt)
         {
             Id = id;
             TicketId = ticketId;
             AttendantId = attendantId;
             AttendantName = attendantName;
+            Role = role;
             Message = message;
             CreatedAt = createdAt;
         }
@@ -18,11 +19,12 @@ namespace EasySupport.Application.Models
         public int TicketId { get; private set; }
         public int AttendantId { get; private set; }
         public string AttendantName { get; set; }
+        public string Role { get; set; }
         public string Message { get; private set; }
         public DateTime CreatedAt { get; private set; }
 
         public static TicketInteractionsViewModel FromEntity(TicketInteraction interaction)
-            => new(interaction.Id, interaction.Ticket.Id, interaction.Attendant.Id, interaction.Attendant.Name, interaction.Message, interaction.CreatedAt);
-                
+            => new(interaction.Id, interaction.Ticket.Id, interaction.Attendant.Id, interaction.Attendant.Name, interaction.Attendant.Role, interaction.Message, interaction.CreatedAt);
+
     }
 }
