af18a2526424a0b59e670e7e8e77476c01a7f2d2
Henrique carvalho <henriqueart7@gmail.com>
2024-12-13 13:26:44 -0300

    criando SolutionTicket

---
+++ EasySupport.Core/Entities/SolutionTicket.cs
@@ -0,0 +1,17 @@
+﻿namespace EasySupport.Core.Entities
+{
+    public class SolutionTicket : BaseEntity
+    {
+        public SolutionTicket(string name) : base()
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public void Update(string name)
+        {
+            Name = name;
+        }
+    }
+}
