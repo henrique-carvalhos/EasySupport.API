ca8f128bce454a80b3925eaf843c5e5b8608e729
Henrique carvalho <henriqueart7@gmail.com>
2024-12-13 13:58:29 -0300

    configurando table SolutionTicket

---
+++ EasySupport.Infrastructure/Persistence/Configurations/SolutionTicketConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using EasySupport.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasySupport.Infrastructure.Persistence.Configurations
+{
+    public class SolutionTicketConfiguration : IEntityTypeConfiguration<SolutionTicket>
+    {
+        public void Configure(EntityTypeBuilder<SolutionTicket> builder)
+        {
+            builder
+                .HasKey(x => x.Id);
+
+            builder
+                .Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(150);
+        }
+    }
+}
