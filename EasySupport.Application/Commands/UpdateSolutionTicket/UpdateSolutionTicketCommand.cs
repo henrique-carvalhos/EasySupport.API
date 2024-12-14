74505add72f74134b5599ffdee913ff3c0a7c3c7
Henrique carvalho <henriqueart7@gmail.com>
2024-12-14 17:36:16 -0300

    implementando UpdateSolutionTicketCommand

---
+++ EasySupport.Application/Commands/UpdateSolutionTicket/UpdateSolutionTicketCommand.cs
@@ -0,0 +1,11 @@
+﻿using EasySupport.Application.Models;
+using MediatR;
+
+namespace EasySupport.Application.Commands.UpdateSolutionTicket
+{
+    public class UpdateSolutionTicketCommand : IRequest<ResultViewModel>
+    {
+        public int SolutionTicketId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ EasySupport.Application/Commands/UpdateSolutionTicket/UpdateSolutionTicketCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using EasySupport.Application.Models;
+using EasySupport.Core.Repositories;
+using MediatR;
+
+namespace EasySupport.Application.Commands.UpdateSolutionTicket
+{
+    public class UpdateSolutionTicketCommandHandler : IRequestHandler<UpdateSolutionTicketCommand, ResultViewModel>
+    {
+        private readonly ISolutionTicketRepository _repository;
+        public UpdateSolutionTicketCommandHandler(ISolutionTicketRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ResultViewModel> Handle(UpdateSolutionTicketCommand request, CancellationToken cancellationToken)
+        {
+            var solution = await _repository.GetByIdAsync(request.SolutionTicketId);
+
+            if (solution is null)
+            {
+                return ResultViewModel.Error("Solução para o ticket não encontrado");
+            }
+
+            solution.Update(request.Name);
+            await _repository.UpdateAsync(solution);
+
+            return ResultViewModel.Success();
+        }
+    }
+}
