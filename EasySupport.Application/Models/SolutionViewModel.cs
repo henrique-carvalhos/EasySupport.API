be83dea045f11819cc020c4e31347b8652275a70
Henrique carvalho <henriqueart7@gmail.com>
2024-12-14 13:44:40 -0300

    criando SolutionViewModel

---
+++ EasySupport.Application/Models/SolutionViewModel.cs
@@ -0,0 +1,21 @@
+﻿using EasySupport.Core.Entities;
+
+namespace EasySupport.Application.Models
+{
+    public class SolutionViewModel
+    {
+        public SolutionViewModel(int id, string name, bool isDeleted)
+        {
+            Id = id;
+            Name = name;
+            IsDeleted = isDeleted;
+        }
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public bool IsDeleted { get; private set; }
+
+        public static SolutionViewModel FromEntity(SolutionTicket solution)
+            => new(solution.Id, solution.Name, solution.IsDeleted);
+    }
+}
