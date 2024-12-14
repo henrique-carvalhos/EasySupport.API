4bce2065321c89afb74ebb60433fb2b95f8ea021
Henrique carvalho <henriqueart7@gmail.com>
2024-12-14 13:21:57 -0300

    implementando ISolutionTicketRepository

---
+++ EasySupport.Core/Repositories/ISolutionTicketRepository.cs
@@ -0,0 +1,12 @@
+﻿using EasySupport.Core.Entities;
+
+namespace EasySupport.Core.Repositories
+{
+    public interface ISolutionTicketRepository
+    {
+        Task<SolutionTicket?> GetByIdAsync(int id);
+        Task<List<SolutionTicket>> GetAllAsync(string search);
+        Task<int> AddAsync(SolutionTicket solution);
+        Task UpdateAsync(SolutionTicket solution);
+    }
+}
