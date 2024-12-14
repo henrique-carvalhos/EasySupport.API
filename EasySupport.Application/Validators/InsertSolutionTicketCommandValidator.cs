7f4e1b0b420eb2e37b23af07901152258e312cc1
Henrique carvalho <henriqueart7@gmail.com>
2024-12-14 17:56:44 -0300

    implementando InsertSolutionTicketCommandValidator

---
+++ EasySupport.Application/Validators/InsertSolutionTicketCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using EasySupport.Application.Commands.InsertSolutionTicket;
+using FluentValidation;
+
+namespace EasySupport.Application.Validators
+{
+    public class InsertSolutionTicketCommandValidator : AbstractValidator<InsertSolutionTicketCommand>
+    {
+        public InsertSolutionTicketCommandValidator()
+        {
+            RuleFor(d => d.Name)
+                .NotEmpty()
+                .WithMessage("Nome não pode ser vazio")
+                .MaximumLength(150)
+                .WithMessage("Tamanho máximo é de 150 caracteres");
+        }
+    }
+}
