bee063e93cfe23db31a7f13794e486e852a72f25
Henrique carvalho <henriqueart7@gmail.com>
2024-12-14 13:20:09 -0300

    criando SolutionTicketsController

---
+++ EasySupport.API/Controllers/SolutionTicketsController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasySupport.API.Controllers
+{
+    [Route("api/solutionTicket")]
+    [ApiController]
+    public class SolutionTicketsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public SolutionTicketsController(IMediator mediator )
+        {
+            _mediator = mediator;
+        }
+    }
+}
