193a756f41f03a48fb55c43000d60207a64843b7
Henrique carvalho <henriqueart7@gmail.com>
2024-12-18 19:54:50 -0300

    adicionando Password ao User

--- EasySupport.Core/Entities/User.cs
+++ EasySupport.Core/Entities/User.cs
@@ -2,11 +2,12 @@
 {
     public class User : BaseEntity
     {
-        public User(string name, string email, string role, DateTime alteredAt, int enterpriseId, int departmentId) : base()
+        public User(string name, string email, string role, string password,DateTime alteredAt, int enterpriseId, int departmentId) : base()
         {
             Name = name;
             Email = email;
             Role = role;
+            Password = password;
             AlteredAt = DateTime.Now;
             EnterpriseId = enterpriseId;
             DepartmentId = departmentId;
@@ -15,6 +16,7 @@
         public string Name { get; private set; }
         public string Email { get; private set; }
         public string Role { get; private set; } // "Atendente" ou "Solicitante"
+        public string Password { get; private set; }
         public DateTime AlteredAt { get; private set; }
 
         public int EnterpriseId { get; private set; }
