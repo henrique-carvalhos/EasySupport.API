ebdcea2370d465722c2db650ddb5369aa0beac2d
Henrique carvalho <henriqueart7@gmail.com>
2024-12-18 19:55:52 -0300

    implementando LoginUserViewModel

---
+++ EasySupport.Application/Models/LoginUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace EasySupport.Application.Models
+{
+    public class LoginUserViewModel
+    {
+        public LoginUserViewModel(string email, string token)
+        {
+            Email = email;
+            Token = token;
+        }
+
+        public string Email { get; private set; }
+        public string Token { get; private set; }
+    }
+}
