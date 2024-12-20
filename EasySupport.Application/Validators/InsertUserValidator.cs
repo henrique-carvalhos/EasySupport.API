6a42774d1f5379ec2ef93c6af1845b99f6904a12
Henrique carvalho <henriqueart7@gmail.com>
2024-12-20 12:09:07 -0300

    adicionando validação do password

--- EasySupport.Application/Validators/InsertUserValidator.cs
+++ EasySupport.Application/Validators/InsertUserValidator.cs
@@ -1,5 +1,6 @@
 ﻿using EasySupport.Application.Commands.InsertUser;
 using FluentValidation;
+using System.Text.RegularExpressions;
 
 namespace EasySupport.Application.Validators
 {
@@ -25,6 +26,18 @@ namespace EasySupport.Application.Validators
                 .WithMessage("Role não pode ser vazio")
                 .MaximumLength(50)
                 .WithMessage("Tamanho máximo é de 50 caracteres");
+
+            RuleFor(p => p.Password)
+                .Must(ValidPassword)
+                .WithMessage("Senha deve conter pelo menos 8 caracteres, um número, uma letra maiúscula, uma minúscula, e um caractere especial");
+
+        }
+
+        public bool ValidPassword(string password)
+        {
+            var regex = new Regex(@"^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$");
+
+            return regex.IsMatch(password);
         }
     }
 }
