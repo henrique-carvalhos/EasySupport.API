fe00952f18085b0743e65d9a47449e77db12f386
Henrique carvalho <henriqueart7@gmail.com>
2024-12-22 20:29:28 -0300

    add migration UpdateDescriptioConfiguration

---
+++ EasySupport.Infrastructure/Persistence/Migrations/20241222213602_UpdateDescriptionConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasySupport.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDescriptionConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tickets",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tickets",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
