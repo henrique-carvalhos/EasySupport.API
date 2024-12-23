358e008081c09e88d12035f7d90219ac76aa217a
Henrique carvalho <henriqueart7@gmail.com>
2024-12-23 16:50:17 -0300

    refatorando serviço de ClientNotification que estava na camada Application

---
+++ EasySupport.Infrastructure/Notifications/TicketCreated/ClientNotificationHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace EasySupport.Infrastructure.Notifications.TicketCreated
+{
+    public class ClientNotificationHandler : INotificationHandler<TicketCreatedNotification>
+    {
+        private readonly ISendGridClient _sendGridClient;
+        public ClientNotificationHandler(ISendGridClient sendGridClient)
+        {
+            _sendGridClient = sendGridClient;
+        }
+
+        public async Task Handle(TicketCreatedNotification notification, CancellationToken cancellationToken)
+        {
+            var message = new SendGridMessage
+            {
+                From = new EmailAddress("xanol55376@ckuer.com", "EasySupport"),
+                Subject = $"Aberto: Ticket Nº: #{notification.Id}",
+                HtmlContent = $@"
+                    <strong>Olá {notification.NameClient}, tudo bem?</strong><br>
+                    <hr>
+                    <strong>Ticket #{notification.Id} - Assunto: {notification.NameCategory} - {notification.NameSubcategory}</strong><br>
+                    <strong>Descrição:</strong> {notification.Description}<br>
+                    <strong>Data criação:</strong> {notification.CreatedAt}"
+            };
+
+            message.AddTo(notification.SenderEmail, notification.NameClient);
+            message.AddCc(message.From);
+
+            await _sendGridClient.SendEmailAsync(message);
+        }
+    }
+}
---
+++ EasySupport.Infrastructure/Notifications/TicketCreated/TicketCreatedNotification.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+
+namespace EasySupport.Infrastructure.Notifications.TicketCreated
+{
+    public class TicketCreatedNotification : INotification
+    {
+        public TicketCreatedNotification(int id, DateTime createdAt, string nameClient, string senderEmail, string nameCategory, string nameSubcategory, string description)
+        {
+            Id = id;
+            CreatedAt = createdAt;
+            NameClient = nameClient;
+            SenderEmail = senderEmail;
+            NameCategory = nameCategory;
+            NameSubcategory = nameSubcategory;
+            Description = description;
+        }
+
+        public int Id { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public string NameClient { get; private set; }
+        public string SenderEmail { get; private set; }
+        public string NameCategory { get; private set; }
+        public string NameSubcategory { get; private set; }
+        public string Description { get; private set; }
+    }
+}
