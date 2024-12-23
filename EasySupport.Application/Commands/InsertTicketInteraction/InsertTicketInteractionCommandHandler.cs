5bebef69184624088cdd08f3788175cfe0cd0806
Henrique carvalho <henriqueart7@gmail.com>
2024-12-23 16:47:18 -0300

    refatorando InsertTicketInteractionCommandHandler para utilizar o INotification

--- EasySupport.Application/Commands/InsertTicketInteraction/InsertTicketInteractionCommandHandler.cs
+++ EasySupport.Application/Commands/InsertTicketInteraction/InsertTicketInteractionCommandHandler.cs
@@ -1,6 +1,6 @@
 ﻿using EasySupport.Application.Models;
-using EasySupport.Application.Notification.TicketInteractionCreated;
 using EasySupport.Core.Repositories;
+using EasySupport.Core.Services;
 using MediatR;
 
 namespace EasySupport.Application.Commands.InsertTicketInteraction
@@ -9,12 +9,12 @@ namespace EasySupport.Application.Commands.InsertTicketInteraction
     {
         private readonly ITicketInteractionRepository _repository;
         private readonly ITicketRepository _ticketRepository;
-        private readonly IMediator _mediator;   
-        public InsertTicketInteractionCommandHandler(ITicketInteractionRepository repository, ITicketRepository ticketRepository, IMediator mediator)
+        private readonly INotificationService _notificationService;
+        public InsertTicketInteractionCommandHandler(ITicketInteractionRepository repository, ITicketRepository ticketRepository, INotificationService notificationService)
         {
             _repository = repository;
             _ticketRepository = ticketRepository;
-            _mediator = mediator;
+            _notificationService = notificationService;
         }
 
         public async Task<ResultViewModel<int>> Handle(InsertTicketInteractionCommand request, CancellationToken cancellationToken)
@@ -39,26 +39,10 @@ namespace EasySupport.Application.Commands.InsertTicketInteraction
                 await _ticketRepository.UpdateAsync(ticketResult);
             }
 
-            var ticket = new TicketInteractionNotification(
-                interaction.Id,
-                ticketResult.Id,
-                ticketResult.Description,
-                ticketResult.StatusTicket.Name,
-                ticketResult.Category.Name,
-                ticketResult.Subcategory.Name,
-                ticketResult.Attendant.Id,
-                ticketResult.Attendant.Name,
-                ticketResult.Attendant.Email,
-                ticketResult.Client.Name,
-                ticketResult.Client.Email,
-                interaction.Message,
-                ticketResult.SolutionTicket.Name,
-                ticketResult.Attendant.Role,
-                interaction.CreatedAt,
-                ticketResult.CreatedAt,
-                ticketResult.Interactions);
-
-            await _mediator.Publish(ticket);
+            if (ticketResult != null)
+            {
+                await _notificationService.NotifyTicketInteraction(interaction, ticketResult);
+            }
 
             return ResultViewModel<int>.Success(interaction.Id);
         }
